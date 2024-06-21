41623c91855cf123c0198f11f85e8ccbf21c29a2
gerson.fragoso <gersonfragoso.araujo@gmail.com>
2024-06-21 16:07:23 -0300

    fix: fixing methods crud

--- src/main/java/com/example/demo/service/impl/ClienteServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/ClienteServiceImpl.java
@@ -2,9 +2,11 @@ package com.example.demo.service.impl;
 
 import com.example.demo.model.Cliente;
 import com.example.demo.model.ClienteRepository;
+import com.example.demo.model.Endereco;
+import com.example.demo.model.EnderecoRepository;
 import com.example.demo.service.ClienteService;
+import com.example.demo.service.ViaCepService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -14,28 +16,62 @@ public class ClienteServiceImpl implements ClienteService {
 
     @Autowired
     private ClienteRepository clienteRepository;
+    @Autowired
+    private EnderecoRepository enderecoRepository;
+
+    @Autowired
+    private ViaCepService viaCepService;
     @Override
     public Iterable<Cliente> buscarTodos() {
         return clienteRepository.findAll();
     }
 
     @Override
-    public Optional<Cliente> buscarPorId(Long id) {
-        return clienteRepository.findById(id);
+    public Cliente buscarPorId(Long id) {
+        Optional<Cliente> cliente = clienteRepository.findById(id);
+        return cliente.get();
     }
 
     @Override
     public void inserir(Cliente cliente) {
+       /*
         clienteRepository.save(cliente);
+        String cep = cliente.getEndereco().getCep();
+        Endereco endereco = enderecoRepository.findById(cep).orElseGet(()-> {
+            Endereco novoEndereco = viaCepService.consultarCep(cep);
+            enderecoRepository.save(novoEndereco);
+            return novoEndereco;
+        });
+        cliente.setEndereco(endereco);
+        clienteRepository.save(cliente);
+        */
+        SalvarClienteComCep(cliente);
     }
 
     @Override
     public void atualizar(Long id, Cliente cliente) {
+        Optional<Cliente> clienteBd = clienteRepository.findById(id);
+        if (clienteBd.isPresent()){
+            SalvarClienteComCep(cliente);
+        }
 
     }
 
+
     @Override
     public void deletar(Long id) {
+        clienteRepository.deleteById(id);
+
+    }
+    private void SalvarClienteComCep(Cliente cliente){
+        String cep  = cliente.getEndereco().getCep();
+        Endereco endereco = enderecoRepository.findById(cep).orElseGet(() ->{
+            Endereco novoEndereco = viaCepService.consultarCep(cep);
+            enderecoRepository.save(novoEndereco);
+            return novoEndereco;
+        });
+        cliente.setEndereco(endereco);
+        clienteRepository.save(cliente);
 
     }
 }
